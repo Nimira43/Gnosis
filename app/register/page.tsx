d02a833bd744a6d69474e89112bba22ad2cd1737
Nimira43 <n.rabone72@aol.com>
2024-09-05 21:14:01 +0100

    Starting to build Register Card

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,19 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Register() {
   return (
-    <main className="flex justify-center items center min-h-screen">Register Page</main>
+    <main className="flex justify-center items-center min-h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>
+            Register
+          </CardTitle>
+          <CardDescription>
+            Register for a new account
+          </CardDescription>
+        </CardHeader>
+
+      </Card>
+    </main>
   )
 }
