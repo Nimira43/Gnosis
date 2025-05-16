618234897f7b90eea1f26bb3975d7767c32811e4
Nimira43 <n.rabone72@aol.com>
2025-05-16 14:49:55 +0100

    Completed users schema.

--- db/usersSchema.ts
+++ db/usersSchema.ts
@@ -1,5 +1,10 @@
-import { pgTable, serial } from 'drizzle-orm/pg-core'
+import { boolean, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
+  email: text('email').unique(),
+  password: text('password'),
+  createdAt: timestamp('created_at').defaultNow(),
+  twoFactorAuthSecret: text('2fa_secret'),
+  twoFactorAuthActivated: boolean('2fa_activated').default(false)
 })
