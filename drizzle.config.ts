43d7cfe6c374472b8c482987004ccb8d3d809437
Nimira43 <n.rabone72@aol.com>
2025-05-16 15:05:47 +0100

    Successfully pushed schema to Neon database.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   schema: './db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.NEON_DATABASE_URL!
-  }
+    url: process.env.NEON_DATABASE_URL!,
+  },
 })
 
