c38c64050b01a9423ce3ec28b87be9704582438e
Nimira43 <n.rabone72@aol.com>
2025-06-06 22:01:45 +0100

    Brought over register form markup into Register - will convert this code.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,71 +1,19 @@
-'use client'
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from '@/components/ui/card';
+import { FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
+import Link from 'next/link';
+import { Input } from 'postcss';
+import { Form } from 'react-hook-form';
 
-import { Button } from '@/components/ui/button'
-import { 
-  Card, 
-  CardContent, 
-  CardDescription, 
-  CardHeader, 
-  CardTitle } from '@/components/ui/card'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { passwordMatchSchema } from '@/validation/passwordMatchSchema'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { registerUser } from './actions'
-import Link from 'next/link'
-
-const formSchema = z.object({
-  email: z.string().email(),
-}).and(passwordMatchSchema)
-
-export default function Register() {
-  
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-      passwordConfirm: ''
-    }
-  })
-
-  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-    const response = await registerUser({
-      email: data.email,
-      password: data.password,
-      passwordConfirm: data.passwordConfirm
-    })
-
-    if (response?.error) {
-      form.setError('email', {
-        message: response?.message
-      })
-    }
-    console.log(response)
-  }
-
+export default function Login() {
   return (
+  
     <main className='flex justify-center items-center min-h-screen'>
-      {form.formState.isSubmitSuccessful ? (
+      
         <Card className='w-[350px]'>
           <CardHeader>
-            <CardTitle>Your account has been created.</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Button asChild className='w-full'>
-              <Link href='/login'>
-                Login
-              </Link>
-            </Button>
-          </CardContent>
-        </Card>        
-      ) : ( 
-        <Card className='w-[350px]'>
-          <CardHeader>
-            <CardTitle className='uppercase'>Register</CardTitle>
-            <CardDescription>Register for a new account.</CardDescription>
+            <CardTitle className='uppercase'>Login</CardTitle>
+            <CardDescription>Login to your new account.</CardDescription>
           </CardHeader>
           <CardContent>
             <Form {...form}>
@@ -125,7 +73,7 @@ export default function Register() {
             </Form>
           </CardContent>
         </Card>
-      )}
+     
     </main>
   )
 }
