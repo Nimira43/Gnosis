cb86b65beb0e0f637594c717e392ce2c5f3726ec
Nimira43 <n.rabone72@aol.com>
2025-06-06 23:43:01 +0100

    Checking user credentials. If there are errors then errors will be thrown. Otherwise user will be authenticated.

--- auth.ts
+++ auth.ts
@@ -1,8 +1,35 @@
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials' 
+import db from './db/drizzle'
+import { eq } from 'drizzle-orm'
+import { users } from './db/usersSchema'
+import { compare } from 'bcryptjs'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-
+    Credentials({
+      credentials: {
+        email: {},
+        password: {}
+      },
+      async authorize(credentials) {
+        const [user] = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.email as string))
+        if (!user) {
+          throw new Error('Incorrect credentials.')
+        } else {
+          const passwordCorrect = await compare(credentials.password as string, user.password!)
+          if (!passwordCorrect) {
+            throw new Error('Incorrect credentials')
+          }
+        }
+        return {
+          id: user.id.toString(),
+          email: user.email,
+        }
+      }
+    })
   ],
 })
