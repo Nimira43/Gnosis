b7146e707fed8edaa2d04624d59db5afb06a4a88
Nimira43 <n.rabone72@aol.com>
2025-06-07 00:07:39 +0100

    Creating Logout button.

---
+++ app/logout-button/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+export default function LogoutButton() {
+  return (
+    <Button
+      size='sm'
+    >
+      Logout
+    </Button>
+  )
+}
