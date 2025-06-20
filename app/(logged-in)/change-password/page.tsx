bbb92f5cd9bbea23f2e0ae2ae37f4a76206d3378
Nimira43 <n.rabone72@aol.com>
2025-06-20 23:19:46 +0100

    Created outline for ChangePassword

--- app/(logged-in)/change-password/page.tsx
+++ app/(logged-in)/change-password/page.tsx
@@ -1,5 +1,34 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+
 export default function ChangePassword() {
   return (
-    <div>ChangePassword</div>
+    <Card className='bg-light-extra w-[350px]'>
+      <CardHeader>
+        <CardTitle 
+          className='font-medium uppercase text-center'
+        >
+          Change Password
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Current Password</Label>
+        <div className='font-normal text-sm'>
+          BadPasswordPlaceholder
+        </div>
+      </CardContent>
+      <CardContent>
+        <Label>New Password</Label>
+        <div className='font-normal text-sm'>
+          WorstPasswordPlaceholder
+        </div>
+      </CardContent>
+      <CardContent>
+        <Label>Confirm Password</Label>
+        <div className='font-normal text-sm'>
+          WorstPasswordPlaceholder
+        </div>
+      </CardContent>
+    </Card>
   )
 }
