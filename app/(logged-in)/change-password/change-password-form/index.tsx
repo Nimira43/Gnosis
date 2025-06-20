c1476bb95a062762538de00dfc9443bf5b4a705c
Nimira43 <n.rabone72@aol.com>
2025-06-20 23:47:21 +0100

    form - 1

--- app/(logged-in)/change-password/change-password-form/index.tsx
+++ app/(logged-in)/change-password/change-password-form/index.tsx
@@ -2,6 +2,8 @@
 
 import { passwordMatchSchema } from '@/validation/passwordMatchSchema'
 import { passwordSchema } from '@/validation/passwordSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 const formSchema = z.object({
@@ -9,6 +11,10 @@ const formSchema = z.object({
 }).and(passwordMatchSchema)
 
 export default function ChangePasswordForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema)
+  })
+
   return (
     <div>ChangePasswordForm</div>
   )
