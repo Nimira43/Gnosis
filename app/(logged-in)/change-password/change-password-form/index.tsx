04ec6a0b9062abe5c9af994eefa67ec5ed6322b0
Nimira43 <n.rabone72@aol.com>
2025-06-20 23:45:05 +0100

    Importing password schemas

--- app/(logged-in)/change-password/change-password-form/index.tsx
+++ app/(logged-in)/change-password/change-password-form/index.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { passwordMatchSchema } from '@/validation/passwordMatchSchema'
+import { passwordSchema } from '@/validation/passwordSchema'
 import { z } from 'zod'
 
-const formSchema = z.object({})
+const formSchema = z.object({
+  currentPassword: passwordSchema
+}).and(passwordMatchSchema)
 
 export default function ChangePasswordForm() {
   return (
