54e02cadd0c6a7721dd25e47f5aeaf548829edf5
Nimira43 <n.rabone72@aol.com>
2025-06-21 00:12:40 +0100

    Setting up changePassword with props and types

--- app/(logged-in)/change-password/change-password-form/actions.ts
+++ app/(logged-in)/change-password/change-password-form/actions.ts
@@ -1,1 +1,13 @@
 'use server'
+
+export const changePassword = async ({
+  currentPassword,
+  password,
+  passwordConfirm
+}: {
+  currentPassword: string
+  password: string
+  passwordConfirm: string
+}) => {
+
+}
