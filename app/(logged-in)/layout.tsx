0cecbb0788f9ef72f561eee5f53138f848ff22d5
Nimira43 <n.rabone72@aol.com>
2025-06-20 21:25:06 +0100

    Created custom transition hover. Added styling to the two links done so far.

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import LogoutButton from '../logout-button'
+
 export default function LoggedInLayout({
   children,
 }: {
@@ -5,7 +8,29 @@ export default function LoggedInLayout({
 }) {
   return (
     <div className='min-h-screen flex flex-col'>
-      <nav></nav>
+      <nav className='bg-main-light '>
+        <ul>
+          <li>
+            <Link 
+              href='/my-account' 
+              className='uppercase font-medium hover:text-main-dark btn-hover'
+            >
+            My Account
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='/change-password'
+              className='uppercase font-medium hover:text-main-dark btn-hover' 
+            >
+              Change Password
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <LogoutButton />
+        </div>
+      </nav>
       {children}  
     </div>
   )
