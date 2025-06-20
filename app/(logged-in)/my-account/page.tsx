e9475afb46202776021f1d649aa5ae3ea9a47105
Nimira43 <n.rabone72@aol.com>
2025-06-20 22:57:29 +0100

    Displaying the user email in my-account.

--- app/(logged-in)/my-account/page.tsx
+++ app/(logged-in)/my-account/page.tsx
@@ -1,5 +1,25 @@
-export default function MyAccount() {
+import { auth } from '@/auth'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+
+export default async function MyAccount() {
+  const session = await auth()
+  
   return (
-    <div>MyAccount</div>
+    <Card className='bg-light w-[350px]'>
+      <CardHeader>
+        <CardTitle 
+          className='font-medium uppercase text-center'
+        >
+          My Account
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Email Address</Label>
+        <div className='font-normal text-sm'>
+          {session?.user?.email}
+        </div>
+      </CardContent>
+    </Card>
   )
 }
