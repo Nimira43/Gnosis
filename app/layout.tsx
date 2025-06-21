eed4fffcfc10c427e17660c3f96aa8f1f7c1c336
Nimira43 <n.rabone72@aol.com>
2025-06-21 01:39:24 +0100

    Brought Toast into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import { Toaster } from '@/components/ui/toaster';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,6 +21,7 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={poppins.className}>
+        <Toaster />
         {children}
       </body>
     </html>
