9f150f2ed5bb6b6d1992e7cb0567d4c11ee12341
Nimira43 <n.rabone72@aol.com>
2025-06-25 20:55:37 +0100

    Copied the Form from Login

--- app/(logged-out)/password-reset/page.tsx
+++ app/(logged-out)/password-reset/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 
 export default function PasswordReset() {
   return (
@@ -14,7 +16,56 @@ export default function PasswordReset() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <fieldset
+                disabled={form.formState.isSubmitting}
+                className='flex flex-col gap-2'
+              >
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Email
+                      </FormLabel>
+                      <FormControl>
+                        <Input {...field} type='email' />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Password
+                      </FormLabel>
+                      <FormControl>
+                        {/* <Input {...field} type='password' /> */}
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* {!!form.formState.errors.root?.message && ( */}
+                  <FormMessage>
+                    {/* {form.formState.errors.root.message} */}
+                  </FormMessage>
+                )}
+                <Button 
+                  className='uppercase'
+                  type='submit'
+                >
+                  Login
+                </Button>
+              </fieldset>
+            </form>
+          </Form>
       </CardContent>
     </Card>
   )
