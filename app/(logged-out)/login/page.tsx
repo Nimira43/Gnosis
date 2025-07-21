b7e1e08d86976a9a0189c3ff370571a37fe5d4a1
Nimira43 <n.rabone72@aol.com>
2025-07-21 07:31:38 +0100

    Creating new email variable

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -42,6 +42,8 @@ export default function Login() {
     }
   }
 
+  const email = form.getValues('email')
+
   return (
     <main className='flex justify-center items-center min-h-screen'>
       <Card className='w-[350px] bg-light-extra'>
--- app/(logged-out)/password-reset/page.tsx
+++ app/(logged-out)/password-reset/page.tsx
@@ -95,6 +95,5 @@ export default function PasswordReset() {
         </CardFooter>
       </Card>
     </main>
-    
   )
 }
