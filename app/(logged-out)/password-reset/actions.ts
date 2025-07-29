ce3cad4e414c747984631b121accbf922fb824bf
Nimira43 <n.rabone72@aol.com>
2025-07-29 15:53:18 +0100

    Check if user is already logged in.

--- app/(logged-out)/password-reset/actions.ts
+++ app/(logged-out)/password-reset/actions.ts
@@ -1,5 +1,14 @@
 'use server'
 
+import { auth } from '@/auth'
+
 export const passwordReset = async (emailAddress: string) => {
+  const session = await auth()
   
+  if (!!session?.user?.id) {
+    return {
+      error: true,
+      message: 'You are already logged in.'
+    }
+  }
 }
