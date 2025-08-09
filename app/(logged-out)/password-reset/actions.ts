ee7c4dcdba6f462733eff52d9f94e53ec28fa086
Nimira43 <n.rabone72@aol.com>
2025-08-09 19:47:49 +0100

    actions - 1

--- app/(logged-out)/password-reset/actions.ts
+++ app/(logged-out)/password-reset/actions.ts
@@ -1,6 +1,9 @@
 'use server'
 
 import { auth } from '@/auth'
+import db from '@/db/drizzle'
+import { users } from '@/db/usersSchema'
+import { eq } from 'drizzle-orm'
 
 export const passwordReset = async (emailAddress: string) => {
   const session = await auth()
@@ -11,4 +14,11 @@ export const passwordReset = async (emailAddress: string) => {
       message: 'You are already logged in.'
     }
   }
+
+  const [user] = await db
+    .select({
+      id: users.id,
+    })
+    .from(users)
+    .where(eq(users.email, emailAddress))
 }
