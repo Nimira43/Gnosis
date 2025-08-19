5c9c8fa55db4b3702e888d9621bdc506d0f0b513
Nimira43 <n.rabone72@aol.com>
2025-08-19 10:07:26 +0100

    Setting up terniary where with the password reset form is shown or a password rest message is shown.

--- app/(logged-out)/password-reset/page.tsx
+++ app/(logged-out)/password-reset/page.tsx
@@ -30,74 +30,81 @@ export default function PasswordReset() {
 
   return (
     <main className='flex justify-center items-center min-h-screen'>
-    {form.formState.isSubmitSuccessful}
-      <Card className='w-[350px]'>
-        <CardHeader>
-          <CardTitle className='uppercase'>
-            Password Reset
-          </CardTitle>
-          <CardDescription>
-            Enter your email address to reset your password.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-              <form onSubmit={form.handleSubmit(handleSubmit)}>
-                <fieldset
-                  disabled={form.formState.isSubmitting}
-                  className='flex flex-col gap-2'
-                >
-                  <FormField
-                    control={form.control}
-                    name='email'
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>
-                          Email
-                        </FormLabel>
-                        <FormControl>
-                          <Input {...field} type='email' />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  {!!form.formState.errors.root?.message && (
-                    <FormMessage>
-                      {form.formState.errors.root.message}
-                    </FormMessage>
-                  )}
-                  <Button 
-                    className='uppercase'
-                    type='submit'
+      {form.formState.isSubmitSuccessful ? ( 
+        <div>
+
+        </div>
+
+      ) : (
+   
+        <Card className='w-[350px]'>
+          <CardHeader>
+            <CardTitle className='uppercase'>
+              Password Reset
+            </CardTitle>
+            <CardDescription>
+              Enter your email address to reset your password.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSubmit)}>
+                  <fieldset
+                    disabled={form.formState.isSubmitting}
+                    className='flex flex-col gap-2'
                   >
-                    Submit
-                  </Button>
-                </fieldset>
-              </form>
-            </Form>
-        </CardContent>
-        <CardFooter className='flex flex-col gap-2'>
-          <div className='text-muted-foreground text-sm'>
-            Remember your password?{' '}
-            <Link 
-              href='/login' 
-              className='text-muted-foreground uppercase'
-            > 
-              Login     
-            </Link>
-          </div>
-          <div className='text-muted-foreground text-sm'>
-            {"Don\'t have an account? "}
-            <Link 
-              href='/login' 
-              className='text-muted-foreground uppercase'
-            > 
-              Register     
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+                    <FormField
+                      control={form.control}
+                      name='email'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>
+                            Email
+                          </FormLabel>
+                          <FormControl>
+                            <Input {...field} type='email' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    {!!form.formState.errors.root?.message && (
+                      <FormMessage>
+                        {form.formState.errors.root.message}
+                      </FormMessage>
+                    )}
+                    <Button 
+                      className='uppercase'
+                      type='submit'
+                    >
+                      Submit
+                    </Button>
+                  </fieldset>
+                </form>
+              </Form>
+          </CardContent>
+          <CardFooter className='flex flex-col gap-2'>
+            <div className='text-muted-foreground text-sm'>
+              Remember your password?{' '}
+              <Link 
+                href='/login' 
+                className='text-muted-foreground uppercase'
+              > 
+                Login     
+              </Link>
+            </div>
+            <div className='text-muted-foreground text-sm'>
+              {"Don\'t have an account? "}
+              <Link 
+                href='/login' 
+                className='text-muted-foreground uppercase'
+              > 
+                Register     
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}   
     </main>
   )
 }
