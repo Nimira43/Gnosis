7e1e48e507252259a5ca7ba29e6921e21c6ad6c1
Nimira43 <n.rabone72@aol.com>
2025-09-12 21:41:11 +0100

    UpdatePassword - 3

--- app/(logged-out)/update-password/page.tsx
+++ app/(logged-out)/update-password/page.tsx
@@ -1,10 +1,29 @@
-export default function UpdatePassword({
+import db from '@/db/drizzle'
+import { passwordResetTokens } from '@/db/passwordResetTokensSchema'
+import { eq } from 'drizzle-orm'
+
+export default async function UpdatePassword({
   searchParams
 }: {
   searchParams: {
     token?: string
   }
 }) {
+  let tokenIsValid = false
+  const {token} = searchParams
+
+  if (token) {
+    const [passwordResetToken] = await db
+      .select()
+      .from(passwordResetTokens)
+      .where(
+        eq(
+          passwordResetTokens
+            .token, token
+        )
+      )
+  }
+
   return (
     <div>UpdatePassword</div>
   )
