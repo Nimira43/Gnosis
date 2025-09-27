c7f3e60f00eabc29e399747ce36257fc55458c30
Nimira43 <n.rabone72@aol.com>
2025-09-27 17:02:46 +0100

    mailer - 2

--- lib/email.ts
+++ lib/email.ts
@@ -1,5 +1,10 @@
 import nodemailer, { createTransport } from 'nodemailer'
 
 export const mailer = createTransport({
-  
+  host: 'smtp.resend.com',
+  port: 587,
+  auth: {
+    user: 'resend',
+    pass: 'process.env.RESEND_API_KEY',
+  }
 })
