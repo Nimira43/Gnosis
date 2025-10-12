5ade04bcc2f291d4e5dcb30ece76f2b1ae3f724d
Nimira43 <n.rabone72@aol.com>
2025-10-12 21:45:55 +0100

    get2faSecret - 1

--- app/(logged-in)/my-account/two-factor-auth-form/actions.ts
+++ app/(logged-in)/my-account/two-factor-auth-form/actions.ts
@@ -1,2 +1,15 @@
 'use server'
 
+import { auth } from "@/auth"
+
+export const get2faSecret = async () => {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return {
+      error: 'true',
+      message: 'Unauthorised'
+    }
+
+  }
+}
