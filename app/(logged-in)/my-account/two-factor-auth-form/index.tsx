168f9cba6095cc46388d00a5b82885f57177a6c3
Nimira43 <n.rabone72@aol.com>
2025-10-12 23:04:25 +0100

    Update TwoFactorAuthForm - 2

--- app/(logged-in)/my-account/two-factor-auth-form/index.tsx
+++ app/(logged-in)/my-account/two-factor-auth-form/index.tsx
@@ -3,22 +3,28 @@
 import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 import { get2faSecret } from './actions'
+import { useToast } from '@/hooks/use-toast'
 
 type Props = {
   twoFactorActivated: boolean
 }
 
 export default function TwoFactorAuthForm({twoFactorActivated} : Props) {
+  const {toast} = useToast()
   const [isActivated, setIsActivated] = useState(twoFactorActivated)
   const [step, setStep] = useState(1)
 
   const handleEnableClick = async () => {
-    setStep(2)
     const response = await get2faSecret()
 
     if (response.error) {
-      
+      toast({
+        variant: 'destructive',
+        title: response.message
+      })
+      return
     }
+    setStep(2)
   }
 
   return (
