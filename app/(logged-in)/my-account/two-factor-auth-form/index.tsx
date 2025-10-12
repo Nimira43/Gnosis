776ae68d41a45e7bf73d2e7f9ff499f99dccedff
Nimira43 <n.rabone72@aol.com>
2025-10-12 21:28:39 +0100

    Enabling 2FA - 5

--- app/(logged-in)/my-account/two-factor-auth-form/index.tsx
+++ app/(logged-in)/my-account/two-factor-auth-form/index.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import { useState } from 'react'
+
 type Props = {
   twoFactorActivated: boolean
 }
 
 export default function TwoFactorAuthForm({twoFactorActivated} : Props) {
+  const [isActivated, setIsActivated] = useState
+
   return (
     <div>2FA Form</div>
   )
