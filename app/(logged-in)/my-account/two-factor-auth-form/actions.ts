59c285069762a5db4d9346399c293a64fcb54830
Nimira43 <n.rabone72@aol.com>
2025-10-12 21:48:46 +0100

    get2faSecret - 2

--- app/(logged-in)/my-account/two-factor-auth-form/actions.ts
+++ app/(logged-in)/my-account/two-factor-auth-form/actions.ts
@@ -1,6 +1,9 @@
 'use server'
 
 import { auth } from "@/auth"
+import db from "@/db/drizzle"
+import { users } from "@/db/usersSchema"
+import { eq } from "drizzle-orm"
 
 export const get2faSecret = async () => {
   const session = await auth()
@@ -10,6 +13,9 @@ export const get2faSecret = async () => {
       error: 'true',
       message: 'Unauthorised'
     }
-
   }
+
+  const [user] = await db.select({
+    twoFactorSecret: users.twoFactorSecret,
+  }).from(users).where(eq(users.id, parseInt(session.user.id)))
 }
