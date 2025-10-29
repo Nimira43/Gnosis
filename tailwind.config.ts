683a5d3cfb60bdc82484e9053c211f02c86eccf3
Nimira43 <n.rabone72@aol.com>
2025-10-29 03:42:11 +0000

    Added caret-blink keyframe to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,6 +74,10 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'caret-blink': {
+          '0%,70%,100%': { opacity: '1' },
+          '20%,50%': { opacity: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
