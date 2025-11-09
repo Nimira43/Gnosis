d66093e2542b91c447d8955b56ebbe43d63451cc
Nimira43 <n.rabone72@aol.com>
2025-11-09 10:01:58 +0000

    Designed simple landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      <h1>NimiraTech Login</h1>
+    <div className='flex items-center justify-center min-h-screen bg-light'>
+      <div className='text-center space-y-6'>
+        <h1 className='text-5xl text-dark logo'>Gnosis</h1>
+        <Link
+          href='/login'
+          className='inline-block font-medium uppercase px-6 py-3 bg-dark text-light rounded'
+        >
+          Go to Login
+        </Link>
+      </div>
     </div>
   )
 }
