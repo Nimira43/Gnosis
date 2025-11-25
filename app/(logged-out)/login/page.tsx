a6b21d05cc3efd2df9413d8985774cca4696432b
Nimira43 <n.rabone72@aol.com>
2025-11-25 18:37:08 +0000

    Copied Step 1 code and placed into Step 2 for updating.

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -154,7 +154,96 @@ export default function Login() {
           </CardFooter>
         </Card>
       )}
-      
+      {step === 2 && (
+        <Card className='w-[350px] bg-grey-light'>
+          <CardHeader>
+            <CardTitle className='uppercase'>
+              Login
+            </CardTitle>
+            <CardDescription>
+              Login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  disabled={form.formState.isSubmitting}
+                  className='flex flex-col gap-2'
+                >
+                  <FormField
+                    control={form.control}
+                    name='email'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Email
+                        </FormLabel>
+                        <FormControl>
+                          <Input {...field} type='email' />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name='password'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Password
+                        </FormLabel>
+                        <FormControl>
+                          <Input {...field} type='password' />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {!!form.formState.errors.root?.message && (
+                    <FormMessage>
+                      {form.formState.errors.root.message}
+                    </FormMessage>
+                  )}
+                  <Button 
+                    className='uppercase'
+                    type='submit'
+                  >
+                    Login
+                  </Button>
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className='flex-col gap-2'>
+            <div className='text-muted-foreground text-sm'>
+              Do not have an account?{' '}
+              <Link
+              className='text-muted-foreground uppercase' 
+                href='/register'
+                >
+                  Register
+                </Link>
+            </div>
+            <div className='text-muted-foreground text-sm'>
+              Forgot password?{' '}
+              <Link 
+                href={`
+                  /password-reset${
+                    email 
+                      ? `?email=${encodeURIComponent(email)}` 
+                      : "" 
+                  }
+                `}
+                className='text-muted-foreground uppercase'
+              >
+                Reset 
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}
     </main>
   )
 }
